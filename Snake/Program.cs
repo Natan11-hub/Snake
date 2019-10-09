5f9525fa7ff318cea48a92563bad122753a21f77
Natan11 <phartynov@mail.ru>
2019-10-09 21:09:31 +0700

    Class List

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -17,6 +17,19 @@ namespace Snake
             Point p2 = new Point(2,7,'*');
             p2.Draw();
 
+            Point p3 = new Point(5, 12, '@');
+            p3.Draw();
+
+            Point p4 = new Point(10, 3, '&');
+            p4.Draw();
+
+            List<Point> valList = new List<Point>();
+
+            valList.Add(p1);
+            valList.Add(p2);
+            valList.Add(p3);
+            valList.Add(p4);
+
             Console.ReadLine();
         }
     }
