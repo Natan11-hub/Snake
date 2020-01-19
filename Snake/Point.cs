e3b38146ef20048e2f5f099076b41ee44d967011
Natan11 <phartynov@mail.ru>
2020-01-19 19:53:29 +0700

    Snake moving!

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -49,6 +49,11 @@ namespace Snake
             Console.SetCursorPosition(x, y);
             Console.Write(sym);           
         }
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
         public override string ToString()
         {
             return x + ", " + y + ", " + sym;
